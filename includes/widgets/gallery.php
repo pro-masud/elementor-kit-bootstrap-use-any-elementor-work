94a751c545c78b01faf4736f86323fab9da2cea0
pro-mannan <promasudbd@gmail.com>
2023-10-21 11:58:45 +0600

    gallery image adding and front-end showing complete

---
+++ includes/widgets/gallery.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Elementor List Widget.
+ *
+ * Elementor widget that inserts an embbedable content into the page, from any given URL.
+ *
+ * @since 1.0.0
+ */
+class Gallery extends \Elementor\Widget_Base {
+
+	/**
+	 * Get widget name.
+	 *
+	 * Retrieve list widget name.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'gallery';
+	}
+
+	/**
+	 * Get widget title.
+	 *
+	 * Retrieve list widget title.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return esc_html__( 'Elementor Test Gallery Images', 'elementor-test' );
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * Retrieve list widget icon.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-gallery-grid';
+	}
+
+	/**
+	 * Get custom help URL.
+	 *
+	 * Retrieve a URL where the user can get more information about the widget.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Widget help URL.
+	 */
+	public function get_custom_help_url() {
+		return 'https://developers.elementor.com/docs/widgets/';
+	}
+
+	/**
+	 * Get widget categories.
+	 *
+	 * Retrieve the list of categories the list widget belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'elementor-test-cat' ];
+	}
+
+	/**
+	 * Get widget keywords.
+	 *
+	 * Retrieve the list of keywords the list widget belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return array Widget keywords.
+	 */
+	public function get_keywords() {
+		return [ 'elementor', 'image', 'gallery', 'photo' ];
+	}
+
+	/**
+	 * Register list widget controls.
+	 *
+	 * Add input fields to allow the user to customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function register_controls() {
+
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => esc_html__( 'Elementor Test Widgets', 'elementor-test' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+
+		$this->add_control(
+			'elementor_gallery',
+			[
+				'label' => esc_html__( 'Gallery Images', 'elementor-test' ),
+				'type' => \Elementor\Controls_Manager::GALLERY,
+				'label_block' => true,
+            ]
+		);
+
+		$this->end_controls_section();
+
+	}
+
+	/**
+	 * Render list widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+        $galleryImages = $settings['elementor_gallery'];
+        // echo "<pre>";
+        // print_r($settings['elementor_gallery']);
+        // echo "</pre>";
+        foreach($galleryImages as $singleImage){
+          echo wp_get_attachment_image($singleImage['id'], 'medium');
+        }
+        
+
+	}
+
+	/**
+	 * Render list widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function content_template() {
+		?>
+        <#
+        _.each( settings.elementor_gallery, function( item ) {
+                var item = {
+                    id:item.id,
+                    url:item.url,
+                    size:'thumbnail'
+                }
+                var galleryUrl = elementor.imagesManager.getImageUrl(item)
+                #>
+                    <img  src="{{galleryUrl}}" alt="">
+                <#
+            });
+            
+        #>
+       
+           
+		<?php
+	}
+
+}
